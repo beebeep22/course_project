880a0989fc16392aa534055c658ca5ee66fa1108
zireael1272 <kseniagonsharenko2007@gmail.com>
2023-11-19 01:12:21 +0200

    Add interface create notification

--- курсовая/forms/create_notification.Designer.cs
+++ курсовая/forms/create_notification.Designer.cs
@@ -29,27 +29,43 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(create_notification));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.content_notification = new System.Windows.Forms.TextBox();
             this.send = new System.Windows.Forms.Button();
             this.tltnotification = new System.Windows.Forms.Label();
+            this.themenotification = new System.Windows.Forms.TextBox();
+            this.tltthemenot = new System.Windows.Forms.Label();
+            this.region = new System.Windows.Forms.ComboBox();
+            this.pathdiseas = new System.Windows.Forms.ComboBox();
+            this.invalid = new System.Windows.Forms.ComboBox();
+            this.alergic = new System.Windows.Forms.ComboBox();
+            this.agefrom = new System.Windows.Forms.ComboBox();
+            this.ageupto = new System.Windows.Forms.ComboBox();
+            this.tltage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // content_notification
             // 
-            this.textBox1.Location = new System.Drawing.Point(205, 94);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(391, 244);
-            this.textBox1.TabIndex = 0;
+            this.content_notification.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.content_notification.Location = new System.Drawing.Point(263, 130);
+            this.content_notification.Multiline = true;
+            this.content_notification.Name = "content_notification";
+            this.content_notification.Size = new System.Drawing.Size(391, 244);
+            this.content_notification.TabIndex = 0;
             // 
             // send
             // 
             this.send.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.send.Location = new System.Drawing.Point(340, 371);
+            this.send.Location = new System.Drawing.Point(404, 396);
             this.send.Name = "send";
             this.send.Size = new System.Drawing.Size(126, 42);
             this.send.TabIndex = 1;
-            this.send.Text = "Відправити";
+            this.send.Text = "Надіслати";
             this.send.UseVisualStyleBackColor = true;
             this.send.Click += new System.EventHandler(this.send_Click);
             // 
@@ -57,24 +73,183 @@
             // 
             this.tltnotification.AutoSize = true;
             this.tltnotification.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.tltnotification.Location = new System.Drawing.Point(205, 72);
+            this.tltnotification.Location = new System.Drawing.Point(260, 109);
             this.tltnotification.Name = "tltnotification";
             this.tltnotification.Size = new System.Drawing.Size(149, 18);
             this.tltnotification.TabIndex = 2;
             this.tltnotification.Text = "Зміст повідомлення";
+            // 
+            // themenotification
+            // 
+            this.themenotification.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.themenotification.Location = new System.Drawing.Point(263, 39);
+            this.themenotification.Multiline = true;
+            this.themenotification.Name = "themenotification";
+            this.themenotification.Size = new System.Drawing.Size(391, 48);
+            this.themenotification.TabIndex = 3;
+            // 
+            // tltthemenot
+            // 
+            this.tltthemenot.AutoSize = true;
+            this.tltthemenot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tltthemenot.Location = new System.Drawing.Point(263, 17);
+            this.tltthemenot.Name = "tltthemenot";
+            this.tltthemenot.Size = new System.Drawing.Size(146, 18);
+            this.tltthemenot.TabIndex = 4;
+            this.tltthemenot.Text = "Тема повідомлення";
+            // 
+            // region
+            // 
+            this.region.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.region.FormattingEnabled = true;
+            this.region.Location = new System.Drawing.Point(44, 130);
+            this.region.Name = "region";
+            this.region.Size = new System.Drawing.Size(151, 24);
+            this.region.TabIndex = 10;
+            // 
+            // pathdiseas
+            // 
+            this.pathdiseas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pathdiseas.FormattingEnabled = true;
+            this.pathdiseas.Location = new System.Drawing.Point(44, 191);
+            this.pathdiseas.Name = "pathdiseas";
+            this.pathdiseas.Size = new System.Drawing.Size(151, 24);
+            this.pathdiseas.TabIndex = 12;
+            // 
+            // invalid
+            // 
+            this.invalid.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.invalid.FormattingEnabled = true;
+            this.invalid.Location = new System.Drawing.Point(44, 265);
+            this.invalid.Name = "invalid";
+            this.invalid.Size = new System.Drawing.Size(151, 24);
+            this.invalid.TabIndex = 14;
+            // 
+            // alergic
+            // 
+            this.alergic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.alergic.FormattingEnabled = true;
+            this.alergic.Location = new System.Drawing.Point(44, 334);
+            this.alergic.Name = "alergic";
+            this.alergic.Size = new System.Drawing.Size(151, 24);
+            this.alergic.TabIndex = 15;
+            // 
+            // agefrom
+            // 
+            this.agefrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.agefrom.FormattingEnabled = true;
+            this.agefrom.Location = new System.Drawing.Point(44, 63);
+            this.agefrom.Name = "agefrom";
+            this.agefrom.Size = new System.Drawing.Size(55, 24);
+            this.agefrom.TabIndex = 16;
+            // 
+            // ageupto
+            // 
+            this.ageupto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ageupto.FormattingEnabled = true;
+            this.ageupto.Location = new System.Drawing.Point(138, 63);
+            this.ageupto.Name = "ageupto";
+            this.ageupto.Size = new System.Drawing.Size(57, 24);
+            this.ageupto.TabIndex = 17;
+            // 
+            // tltage
+            // 
+            this.tltage.AutoSize = true;
+            this.tltage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tltage.Location = new System.Drawing.Point(41, 39);
+            this.tltage.Name = "tltage";
+            this.tltage.Size = new System.Drawing.Size(29, 18);
+            this.tltage.TabIndex = 18;
+            this.tltage.Text = "Вік";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 18);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Від";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(106, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "до";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(41, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 18);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Регіон";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(24, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 18);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Патологічні захворювання";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(41, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 18);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Інвалідність";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(44, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 18);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Алергії";
             // 
             // create_notification
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tltage);
+            this.Controls.Add(this.ageupto);
+            this.Controls.Add(this.agefrom);
+            this.Controls.Add(this.alergic);
+            this.Controls.Add(this.invalid);
+            this.Controls.Add(this.pathdiseas);
+            this.Controls.Add(this.region);
+            this.Controls.Add(this.tltthemenot);
+            this.Controls.Add(this.themenotification);
             this.Controls.Add(this.tltnotification);
             this.Controls.Add(this.send);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.content_notification);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "create_notification";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Створення повідомлення";
+            this.Load += new System.EventHandler(this.create_notification_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,8 +257,23 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox content_notification;
         private System.Windows.Forms.Button send;
         private System.Windows.Forms.Label tltnotification;
+        private System.Windows.Forms.TextBox themenotification;
+        private System.Windows.Forms.Label tltthemenot;
+        private System.Windows.Forms.ComboBox region;
+        private System.Windows.Forms.ComboBox pathdiseas;
+        private System.Windows.Forms.ComboBox invalid;
+        private System.Windows.Forms.ComboBox alergic;
+        private System.Windows.Forms.ComboBox agefrom;
+        private System.Windows.Forms.ComboBox ageupto;
+        private System.Windows.Forms.Label tltage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- курсовая/forms/create_notification.cs
+++ курсовая/forms/create_notification.cs
@@ -16,10 +16,85 @@ namespace курсовая.forms
         {
             InitializeComponent();
         }
-
         private void send_Click(object sender, EventArgs e)
         {
-            this.Hide();
+            //перепроверить в какой момент это должно проверятся
+            if (themenotification.Text == "")
+            {
+                MessageBox.Show("Тема повідомлення не введена.");
+            }
+            else if (content_notification.Text == "")
+            {
+                MessageBox.Show("Опис повідомлення не введен.");
+            }
+            else if (agefrom.SelectedItem != null && ageupto.SelectedItem != null)
+            {
+                int selectedValue1 = Convert.ToInt32(agefrom.SelectedItem);
+                int selectedValue2 = Convert.ToInt32(ageupto.SelectedItem);
+
+                if (selectedValue1 == selectedValue2)
+                {
+                    MessageBox.Show("Некорректно обраний вік.");
+                }
+                else if (selectedValue1 > selectedValue2)
+                {
+                    MessageBox.Show("Некорректно обраний вік.");
+                }
+                else
+                    this.Hide();
+            }
+            else if (agefrom.SelectedItem != null || ageupto.SelectedItem != null)
+            {
+                MessageBox.Show("Оберіть оба значення.");
+            }
+            else if (agefrom.Text == "" && ageupto.Text == "" && region.Text == "" && pathdiseas.Text == "" && invalid.Text == "" && alergic.Text == "")
+            {
+                //не знаю нужно ли
+                MessageBox.Show("Ви не обрали жодного фільтра.");
+            }
+            else if (agefrom.Text != "" || ageupto.Text != "" || region.Text != "" || pathdiseas.Text != "" || invalid.Text != "" || alergic.Text != "")
+            {
+                if (themenotification.Text == "")
+                {
+                    MessageBox.Show("Тема повідомлення не введена.");
+                }
+                else if (content_notification.Text == "")
+                {
+                    MessageBox.Show("Опис повідомлення не введен.");
+                }
+                else
+                    this.Hide();
+            }
+        }
+
+        private void create_notification_Load(object sender, EventArgs e)
+        {
+            for (int i = 16; i <= 99; i++)
+            {
+                agefrom.Items.Add(i);
+                ageupto.Items.Add(i);
+            }
+
+            invalid.Items.Add("-");
+            invalid.Items.Add("I");
+            invalid.Items.Add("II");
+            invalid.Items.Add("III");
+
+            //патологічні захворювання
+            pathdiseas.Items.Add("курсове захворювання");
+
+            //алергії
+            alergic.Items.Add("на курсач");
+
+            //регіон
+            region.Items.Add("Запорізька область");
+            region.Items.Add("Харьківська область");
+            region.Items.Add("Дніпропетровська область");
+            region.Items.Add("Київська область");
+            region.Items.Add("Винницька область");
+            region.Items.Add("Житомирьська область");
+            region.Items.Add("Львівська область");
+
         }
     }
 }

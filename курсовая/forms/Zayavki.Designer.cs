0d88387a965200181490f24da5ed800382f53a52
zireael1272 <kseniagonsharenko2007@gmail.com>
2023-11-19 23:22:22 +0200

    Correct

--- курсовая/forms/Zayavki.Designer.cs
+++ курсовая/forms/Zayavki.Designer.cs
@@ -33,14 +33,14 @@ namespace курсовая.forms
         {
             this.label1 = new System.Windows.Forms.Label();
             this.userRequestsTable = new System.Windows.Forms.DataGridView();
-            this.createnotif = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.filterByRegion = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.requestTopic = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ResponseStatusColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.applicantUsername = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.applicantRegion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.createnotif = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filterByRegion = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.userRequestsTable)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,10 +48,9 @@ namespace курсовая.forms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(93, 33);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(124, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.Size = new System.Drawing.Size(100, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Вхідні заявки";
             // 
@@ -67,56 +66,15 @@ namespace курсовая.forms
             this.ResponseStatusColumn,
             this.applicantUsername,
             this.applicantRegion});
-            this.userRequestsTable.Location = new System.Drawing.Point(22, 61);
-            this.userRequestsTable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.userRequestsTable.Location = new System.Drawing.Point(55, 75);
+            this.userRequestsTable.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
             this.userRequestsTable.Name = "userRequestsTable";
             this.userRequestsTable.RowHeadersWidth = 51;
             this.userRequestsTable.RowTemplate.Height = 24;
             this.userRequestsTable.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.userRequestsTable.Size = new System.Drawing.Size(567, 225);
+            this.userRequestsTable.Size = new System.Drawing.Size(865, 277);
             this.userRequestsTable.TabIndex = 1;
             this.userRequestsTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.userRequestsTable_CellContentClick);
-            // 
-            // createnotif
-            // 
-            this.createnotif.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.createnotif.Location = new System.Drawing.Point(448, 302);
-            this.createnotif.Margin = new System.Windows.Forms.Padding(2);
-            this.createnotif.Name = "createnotif";
-            this.createnotif.Size = new System.Drawing.Size(127, 40);
-            this.createnotif.TabIndex = 3;
-            this.createnotif.Text = "Створити повідомлення";
-            this.createnotif.UseVisualStyleBackColor = true;
-            this.createnotif.Click += new System.EventHandler(this.createnotif_Click);
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(257, 302);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 40);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Відповісти на запит";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // filterByRegion
-            // 
-            this.filterByRegion.FormattingEnabled = true;
-            this.filterByRegion.Location = new System.Drawing.Point(464, 27);
-            this.filterByRegion.Name = "filterByRegion";
-            this.filterByRegion.Size = new System.Drawing.Size(121, 21);
-            this.filterByRegion.TabIndex = 4;
-            this.filterByRegion.SelectedIndexChanged += new System.EventHandler(this.filterByRegion_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(464, 8);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Filter by region";
             // 
             // requestTopic
             // 
@@ -139,32 +97,76 @@ namespace курсовая.forms
             // applicantUsername
             // 
             this.applicantUsername.DataPropertyName = "ApplicantObj";
-            this.applicantUsername.HeaderText = "Юзернейм заявника";
+            this.applicantUsername.HeaderText = "Логін заявника";
             this.applicantUsername.MinimumWidth = 6;
             this.applicantUsername.Name = "applicantUsername";
             this.applicantUsername.ReadOnly = true;
-            this.applicantUsername.Width = 75;
             // 
             // applicantRegion
             // 
             this.applicantRegion.DataPropertyName = "ApplicantObj";
             this.applicantRegion.HeaderText = "Регіон заявника";
+            this.applicantRegion.MinimumWidth = 6;
             this.applicantRegion.Name = "applicantRegion";
             this.applicantRegion.ReadOnly = true;
             this.applicantRegion.Width = 175;
             // 
+            // createnotif
+            // 
+            this.createnotif.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.createnotif.Location = new System.Drawing.Point(584, 379);
+            this.createnotif.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.createnotif.Name = "createnotif";
+            this.createnotif.Size = new System.Drawing.Size(169, 49);
+            this.createnotif.TabIndex = 3;
+            this.createnotif.Text = "Створити повідомлення";
+            this.createnotif.UseVisualStyleBackColor = true;
+            this.createnotif.Click += new System.EventHandler(this.createnotif_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(255, 379);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Відповісти на запит";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filterByRegion
+            // 
+            this.filterByRegion.FormattingEnabled = true;
+            this.filterByRegion.Location = new System.Drawing.Point(703, 40);
+            this.filterByRegion.Margin = new System.Windows.Forms.Padding(4);
+            this.filterByRegion.Name = "filterByRegion";
+            this.filterByRegion.Size = new System.Drawing.Size(160, 24);
+            this.filterByRegion.TabIndex = 4;
+            this.filterByRegion.SelectedIndexChanged += new System.EventHandler(this.filterByRegion_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(700, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Фільтр за регіоном";
+            // 
             // Zayavki
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(936, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.filterByRegion);
             this.Controls.Add(this.createnotif);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.userRequestsTable);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
             this.Name = "Zayavki";
             this.Text = "Zayavki";
             this.Load += new System.EventHandler(this.Zayavki_Load);
